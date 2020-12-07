3acd61b99d84252fff75c68691f41d91b2a96352
jrgleason <jackiegleason@gmail.com>
2020-12-06 23:48:00 -0500

    Finishing up for the night

--- jrg-ui/index.mjs
+++ jrg-ui/index.mjs
@@ -3,11 +3,6 @@ import '@material/mwc-top-app-bar-fixed/mwc-top-app-bar-fixed.js';
 import '@material/mwc-list/mwc-list.js';
 import '@material/mwc-list/mwc-list-item.js';
 import '@material/mwc-icon-button/mwc-icon-button.js'
-
-function CREATE_ELEMENT(name, object, params) {
-    customElements.get(name) ||
-    customElements.define(name, object, params);
-}
 import {StatefulComponent} from "./src/StatefulComponent/index.mjs";
 import {PersonComponent} from "./src/PersonComponent/index.mjs";
 import {BusParticipant} from "./src/Bus.mjs";
@@ -17,18 +12,21 @@ import {TopBarFixed} from "./src/TopBarFixedComponent/index.mjs";
 import {SplashComponent} from "./src/SplashComponent/index.mjs";
 import {MyDrawer} from "./src/MyDrawerComponent/index.mjs";
 
+function CREATE_ELEMENT(name, object, params) {
+    customElements.get(name) ||
+    customElements.define(name, object, params);
+}
 
-window.person = new BusParticipant();
-window.person.subscribe((val)=>{
-    console.log("Next");
-});
-window.person.send("Test");
+const bus = new BusParticipant();
+
+window.jrg_bus = bus;
 document.onreadystatechange = function () {
     let state = document.readyState;
     if (state === 'complete') {
         console.log("loaded");
     }
 };
+
 // Make global instead of individual
 CREATE_ELEMENT("jrg-drawer", MyDrawer, {});
 CREATE_ELEMENT("jrg-stateful", StatefulComponent, {});
@@ -37,3 +35,5 @@ CREATE_ELEMENT('jrg-markdown', MarkdownComponent, {});
 CREATE_ELEMENT('jrg-sidebar', SidebarComponent, {});
 CREATE_ELEMENT('jrg-top-bar-fixed', TopBarFixed, {});
 CREATE_ELEMENT('jrg-splash', SplashComponent, {});
+
+export {CREATE_ELEMENT, bus}
